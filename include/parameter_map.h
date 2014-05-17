40c4ab4297aa0e286f3fde25664e3dae2c51db9d
Xiaochen Fan <fan.daybreak@gmail.com>
2014-05-17 20:42:42 +0800

    merge head files

--- include/parameter_map.h
+++ include/parameter_map.h
@@ -2,8 +2,25 @@
 #define PARAMETER_MAP_H
 
 #include <map>
+#include <Eigen/Dense>
+#include <lemon/list_graph.h>
 
-#include "parameters.h"
+class Parameters
+{
+public:
+    Parameters() {
+        affi_rot_.setZero();
+        affi_trans_.setZero();
+        correspondence_.setZero();
+        weights_.setZero();
+    }
+
+private:
+    Eigen::Matrix3d affi_rot_;
+    Eigen::Vector3d affi_trans_;
+    Eigen::Vector3d correspondence_;
+    Eigen::Vector3d weights_;
+};
 
 class ParameterMap
 {
